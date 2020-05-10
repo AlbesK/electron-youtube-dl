2424f30b546fdec2ce1f8bcf97a7675a61ad0b78
AlbesK <koxhaj19@hotmail.com>
2020-05-10 22:02:23 +0100

    Some more changes, will definitely need to use Electron soon. Thought I could bypass my current problem by trying php, but my understanding is limited and does not seem for today to have helped bridge the gap between the .js and .py file.

--- src/web_ui.js
+++ src/web_ui.js
@@ -1,30 +1,37 @@
 /* Part to be used to call Python backend or to pass the string for call etc*/
 
 //Assign functions to buttons
-document.getElementById("dl").onclick = d;
+console.log("ok");
+document.getElementById("b").onclick = download;
 // <?php echo add(1,2);?> , https://stackoverflow.com/questions/3345457/how-to-put-php-inside-javascript#3345487
 
-function d(){
+function download(){
     // var h = Number(document.getElementById("i_1h").value);  
     // document.getElementById("i_1H").value = h;
- 
-    // var link = document.getElementById("link").value;
-    // var playlist_answers = document.getElementsByName("playlist_answer");
-    // var download_methods = document.getElementsByName("download_method");
-    // var pl_answ = radio_check(playlist_answers);
-    // var dl_mthd = radio_check(download_methods);
-    document.getElementById("output").innerHTML = 5 + 6;
+    var link = document.getElementById("link").value;
+    var playlist_answers = document.getElementsByName("playlist_answer");
+    var download_methods = document.getElementsByName("download_method");
+    var pl_answ = radio_check(playlist_answers);
+    var dl_mthd = radio_check(download_methods);
+    // document.getElementById("output").innerHTML = pl_answ;
+    var command = "python3 mpX.py "+link + " " + pl_answ
     
-    // var run_python = "<?php $command = escapeshellcmd('python3 /usr/custom/test.py'); $output = shell_exec($command); echo $output;?>"
-    
+    var run_python = "<?php $command = escapeshellcmd(ls); $output = shell_exec($command); echo $output;?>"
+    alert(run_python)
   }
 
-function radio_check(radios){
-  var radio_value;
-  for(var i = 0; i < radios.length; i++){
-    if(rates[i].checked){
-      radio_value = radios[i].value;
-    }  
+  function radio_check(radios){
+    var radio_value;
+    
+    for(var i = 0; i < radios.length; i++){
+      // alert("ok");
+      if(radios[i].checked){
+        // alert("cool");
+        radio_value = radios[i].value;
+        break;
+      }  
+      
+    }
+    
+    return radio_value
   }
-  return radio_value
-}
