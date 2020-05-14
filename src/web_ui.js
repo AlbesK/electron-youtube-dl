1b5f9a27cc99f8d62d6b85ed131d131a3b079ed5
AlbesK <koxhaj19@hotmail.com>
2020-05-14 22:44:41 +0100

    Still trying to make ajax work on my own before using node/electron. Will try using my own machine as a server with the mindset of perhaps using the request package of python to listen for events from the main javascript file. Only when I am done with this hopefully will I use node/electorn for the 2nd version

--- src/web_ui.js
+++ src/web_ui.js
@@ -1,7 +1,7 @@
 /* Part to be used to call Python backend or to pass the string for call etc*/
 
 //Assign functions to buttons
-console.log("ok");
+// console.log("ok");
 document.getElementById("b").onclick = download;
 // <?php echo add(1,2);?> , https://stackoverflow.com/questions/3345457/how-to-put-php-inside-javascript#3345487
 
@@ -14,24 +14,38 @@ function download(){
     var pl_answ = radio_check(playlist_answers);
     var dl_mthd = radio_check(download_methods);
     // document.getElementById("output").innerHTML = pl_answ;
-    var command = "python3 mpX.py "+link + " " + pl_answ
-    
-    var run_python = "<?php $command = escapeshellcmd(ls); $output = shell_exec($command); echo $output;?>"
-    alert(run_python)
-  }
+    var command = "python3 mpX.py "+link + " " + pl_answ;
 
-  function radio_check(radios){
-    var radio_value;
+    var run_python = "<?php $command = escapeshellcmd(ls); $output = shell_exec($command); echo $output;?>";
+    alert(run_python);
+    var output = call_python();
+    alert("Call Python");
+}
+
+function radio_check(radios){
+  var radio_value;
+  
+  for(var i = 0; i < radios.length; i++){
+    // alert("ok");
+    if(radios[i].checked){
+      // alert("cool");
+      radio_value = radios[i].value;
+      break;
+    }  
     
-    for(var i = 0; i < radios.length; i++){
-      // alert("ok");
-      if(radios[i].checked){
-        // alert("cool");
-        radio_value = radios[i].value;
-        break;
-      }  
-      
-    }
-    
-    return radio_value
   }
+  
+  return radio_value;
+}
+
+function call_python(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("output").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("POST", "mpX.py", true);
+  xhttp.send();
+}
